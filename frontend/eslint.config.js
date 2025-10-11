d59f8f19f64c95a00e48515aa6586b30fda353ad
Yehor <ka.egor.ka@gmail.com>
2025-10-11 13:29:53 +0200

    fixed package issue in frontend

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -7,7 +8,7 @@ import prettier from 'eslint-config-prettier'
 
 export default [
   {
-    ignores: ['dist', 'node_modules', '.git'],
+    ignores: ['dist', 'node_modules'],
   },
   {
     files: ['**/*.{ts,tsx,js,jsx}'],
@@ -15,28 +16,41 @@ export default [
       ecmaVersion: 2021,
       sourceType: 'module',
       globals: globals.browser,
-      parser: tseslint.parser,
       parserOptions: {
         ecmaFeatures: { jsx: true },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
       },
     },
   },
   {
     files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+      },
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      reactPlugin.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
-      prettier, // Must be LAST to disable conflicting rules
+      prettier,
     ],
     plugins: {
+      react: reactPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       '@typescript-eslint': tseslint.plugin,
     },
     rules: {
       'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
       '@typescript-eslint/no-unused-vars': [
         'warn',
         { argsIgnorePattern: '^_' },
