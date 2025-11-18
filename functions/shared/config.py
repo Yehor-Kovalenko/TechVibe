6f988cbd2572cdcc3c946aadba1eb29b2733bbba
firaanki <247773@edu.p.lodz.pl>
2025-11-18 14:19:27 +0100

    patches

--- functions/shared/config.py
+++ functions/shared/config.py
@@ -7,7 +7,7 @@ RESULTS_CONTAINER = os.environ.get("RESULTS_CONTAINER", "results")
 STORAGE_CONN = os.environ.get("AzureWebJobsStorage", "UseDevelopmentStorage=true")
 CONNECTION_STRING = os.environ.get("CONNECTION_STRING", "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;")
 
-MAX_DEQUEUE_COUNT = os.environ.get("MAX_DEQUEUE_COUNT", "5")
+MAX_DEQUEUE_COUNT = int(os.environ.get("MAX_DEQUEUE_COUNT", "5"))
 
 # FILENAME CONFIGURATION
 JOB_METADATA_FILENAME = os.environ.get("JOB_METADATA_FILENAME", "job_metadata.json")
